1d46f8df2321b5f0ddedf4ee4b1ddaa3c6b6a39d
J Perrin <perrinjack96@gmail.com>
2020-05-03 15:58:52 +0100

    Environment variables hidden

---
+++ main.rb
@@ -0,0 +1,4 @@
+#Process
+
+require './send_sms.rb'
+send_text("HELLO", ['+447590606244'])
--- send_sms.rb
+++ send_sms.rb
@@ -1,1 +1,22 @@
 #Send SMS Ruby File
+def send_text(message, numbers_to_message)
+    require 'rubygems'
+    require 'twilio-ruby'
+    require 'dotenv/load'
+
+    # Your Account Sid and Auth Token from twilio.com/console
+    # DANGER! This is insecure. See http://twil.io/secure
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN'] 
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    numbers_to_message.each do |number|
+        message = @client.messages.create(
+            body: message ,
+            from: '+447380319669',
+            to: number
+        )
+    end
+
+    puts message.sid
+end
